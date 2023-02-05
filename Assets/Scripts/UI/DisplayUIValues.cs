de3301f19c9cc3c6939ff1a9cba941a79b04bc9c
Miro Aurela <miroaurela@gmail.com>
2023-02-05 13:27:11 +0200

    Started working on disabling the sliders.

--- Assets/Scripts/UI/DisplayUIValues.cs
+++ Assets/Scripts/UI/DisplayUIValues.cs
@@ -10,13 +10,56 @@ public class DisplayUIValues : MonoBehaviour
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] Slider sliderDeathSave;
     [SerializeField] Slider sliderLifeSave;
+    [SerializeField] Image lifeSaveButton, deathSaveButton;
 
     public static event Action<int> OnDeathSaveChange;
     public static event Action<int> OnLifeSaveChange;
 
+    bool lifeSaveOn = true;
+    bool deathSaveOn = true;
+
+    int lifeSaveLastValue, deathSaveLastValue;
+
+    void Start()
+    {
+        lifeSaveLastValue = (int)sliderLifeSave.value;
+        deathSaveLastValue = (int)sliderDeathSave.value;
+    }
+
     public void SetValue(int value)
     {
         text.text = value.ToString();
+    }
+
+    public void SetLifeSaveOn(bool on)
+    {
+        lifeSaveOn = on;
+        sliderLifeSave.interactable = on;
+
+        if (on)
+        {
+            sliderLifeSave.value = lifeSaveLastValue;
+        } else
+        {
+            lifeSaveLastValue = (int) sliderLifeSave.value; 
+            sliderLifeSave.value = 0;
+        }
+    }
+
+    public void SetDeathSaveOn(bool on)
+    {
+        deathSaveOn = on;
+        sliderDeathSave.interactable = on;
+
+        if (on)
+        {
+            sliderDeathSave.value = deathSaveLastValue;
+            if (sliderDeathSave.value > sliderLifeSave.value) sliderDeathSave.value = sliderLifeSave.value;
+        } else
+        {
+            deathSaveLastValue = (int) sliderDeathSave.value;
+            sliderDeathSave.value = 0;
+        }
     }
 
     public void UpdateDeathSave()
